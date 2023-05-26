24b8945bb95a97d2f33bbb0a39b8ebd41b346113
Tio <t6babayemi@gmail.com>
2023-05-26 11:36:34 +0100

    added getters and setters

--- src/main/java/com/weatherapp/myweatherapp/model/CityInfo.java
+++ src/main/java/com/weatherapp/myweatherapp/model/CityInfo.java
@@ -20,6 +20,54 @@ public class CityInfo {
   @JsonProperty("daylightHours")
   long daylightHours;
 
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public CurrentConditions getCurrentConditions() {
+    return currentConditions;
+  }
+
+  public void setCurrentConditions(CurrentConditions currentConditions) {
+    this.currentConditions = currentConditions;
+  }
+
+  public List<Days> getDays() {
+    return days;
+  }
+
+  public void setDays(List<Days> days) {
+    this.days = days;
+  }
+
+  public long getDaylightHours() {
+    return daylightHours;
+  }
+
+  public void setDaylightHours(long daylightHours) {
+    this.daylightHours = daylightHours;
+  }
+
+  public boolean isCurrentlyRaining() {
+    return currentlyRaining;
+  }
+
+  public void setCurrentlyRaining(boolean currentlyRaining) {
+    this.currentlyRaining = currentlyRaining;
+  }
+
   @JsonProperty("currentlyRaining")
   boolean currentlyRaining;
 
@@ -42,6 +90,54 @@ public class CityInfo {
 
     @JsonProperty("conditions")
     String conditions;
+
+    public String getCurrentTemperature() {
+      return currentTemperature;
+    }
+
+    public void setCurrentTemperature(String currentTemperature) {
+      this.currentTemperature = currentTemperature;
+    }
+
+    public String getSunrise() {
+      return sunrise;
+    }
+
+    public void setSunrise(String sunrise) {
+      this.sunrise = sunrise;
+    }
+
+    public String getSunset() {
+      return sunset;
+    }
+
+    public void setSunset(String sunset) {
+      this.sunset = sunset;
+    }
+
+    public String getFeelslike() {
+      return feelslike;
+    }
+
+    public void setFeelslike(String feelslike) {
+      this.feelslike = feelslike;
+    }
+
+    public String getHumidity() {
+      return humidity;
+    }
+
+    public void setHumidity(String humidity) {
+      this.humidity = humidity;
+    }
+
+    public String getConditions() {
+      return conditions;
+    }
+
+    public void setConditions(String conditions) {
+      this.conditions = conditions;
+    }
   }
 
   static class Days {
@@ -64,6 +160,53 @@ public class CityInfo {
     @JsonProperty("description")
     String description;
 
+    public String getDate() {
+      return date;
+    }
+
+    public void setDate(String date) {
+      this.date = date;
+    }
+
+    public String getCurrentTemperature() {
+      return currentTemperature;
+    }
+
+    public void setCurrentTemperature(String currentTemperature) {
+      this.currentTemperature = currentTemperature;
+    }
+
+    public String getMaxTemperature() {
+      return maxTemperature;
+    }
+
+    public void setMaxTemperature(String maxTemperature) {
+      this.maxTemperature = maxTemperature;
+    }
+
+    public String getMinTemperature() {
+      return minTemperature;
+    }
+
+    public void setMinTemperature(String minTemperature) {
+      this.minTemperature = minTemperature;
+    }
+
+    public String getConditions() {
+      return conditions;
+    }
+
+    public void setConditions(String conditions) {
+      this.conditions = conditions;
+    }
+
+    public String getDescription() {
+      return description;
+    }
+
+    public void setDescription(String description) {
+      this.description = description;
+    }
   }
 
 }
